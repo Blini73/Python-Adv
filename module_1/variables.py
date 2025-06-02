6fcde40fc3dd4cd0078aff378bb57b3be67a201e
Blini73 <blinmehmeti10@gmail.com>
2025-06-02 17:50:19 +0200

    module_1

---
+++ module_1/variables.py
@@ -0,0 +1,29 @@
+from email.iterators import body_line_iterator
+
+temperature = 13
+name = "Enes"
+
+#print(temperature)
+print(type(temperature))
+
+print(name)
+print(type(name))
+
+x = 10
+y = 20
+
+#print(x+y)
+
+Z=x+y
+print(x + y)
+
+
+print(x - y)
+
+
+print(x * y)
+
+name = blin
+surname = mehmeti
+print(name + surname)
+
