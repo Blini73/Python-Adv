56879d89d0ca3cd910a59afabe5359730d9c2fb7
Blini73 <blinmehmeti10@gmail.com>
2025-06-04 17:45:53 +0200

    Add files via upload

---
+++ module_2/tuple.py
@@ -0,0 +1,13 @@
+words=('spam','eggs','milk')
+
+print(words[0])
+
+# words[1]="chesse"
+
+# print(words)
+
+person=('Amar',14,'Student')
+
+name,age,profession=person
+
+print(name,"'s","profession is",profession,"and he is",age,"years old")
