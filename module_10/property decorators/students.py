10b7dc02c02f2f92af1ac348a787f49caae931b5
Student <blinmehmeti10@gmail.com>
2025-09-29 17:37:53 +0200

    module_9

---
+++ module_10/property decorators/students.py
@@ -0,0 +1,33 @@
+class Student:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+# Create a student instance
+student1 = Student("Alice", 17)
+
+print("Name: ", student1.name)
+print("Age: ", student1.age)
+
+# Update student info
+student1.name = "Bob"
+student1.age = 19
+
+print("Updated name: ", student1.name)
+print("Updated age: ", student1.age)
